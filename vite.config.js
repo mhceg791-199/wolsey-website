1e39b1be043d650d88e0b66a77044bcf79fe5332
MinaNagy99 <minanagykhalefa@gmail.com>
2025-03-16 23:07:23 +0200

    handel folder for deplyment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  //   build: {
-  //   outDir: "../../var/www/Wolsey", // Replace with your desired directory path
-  // },
+    build: {
+    outDir: "../../var/www/Wolsey", // Replace with your desired directory path
+  },
   plugins: [react()],
 })
