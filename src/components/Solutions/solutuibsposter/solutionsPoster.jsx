62ffc18c5fffd0aa48a1b6a0c9dde91c841deee4
MinaNagy99 <minanagykhalefa@gmail.com>
2025-03-17 13:51:14 +0200

    handel margin for solution title

--- src/components/Solutions/solutuibsposter/solutionsPoster.jsx
+++ src/components/Solutions/solutuibsposter/solutionsPoster.jsx
@@ -7,10 +7,10 @@ function SolutionsPoster() {
         style={{
           backgroundImage: `url("/portfolio/poster.webp")`,
         }}
-        className="poster  mt-24 "
+        className="poster  mt-10 "
       >
         <div className="flex flex-col justify-start items-center p-5 md:h-full">
-          <h1 className="custom-text-xxl  md:mt-32 mt-30">
+          <h1 className="custom-text-xxl  md:mt-20 mt-30">
             Structural Engineering Solutions
           </h1>
         </div>
