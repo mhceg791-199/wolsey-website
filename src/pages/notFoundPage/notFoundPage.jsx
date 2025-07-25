14b69aad0260788e6587f1a3cf77624d224ae69a
minanagy99 <minanagykhalefa@gmail.com>
2025-07-25 18:41:07 +0300

    handle 404

---
+++ src/pages/notFoundPage/notFoundPage.jsx
@@ -0,0 +1,11 @@
+// src/pages/NotFoundPage/NotFoundPage.js
+
+const NotFoundPage = () => {
+    return (
+        <div >
+            <img className="h-screen w-full object-contain" src="/notFoundPage/notfound.webp" alt="" />
+        </div>
+    );
+};
+
+export default NotFoundPage;
