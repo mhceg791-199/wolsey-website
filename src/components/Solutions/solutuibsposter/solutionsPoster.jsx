0ab5c1323a2f8d4c76b141a1ea32a0b02cc6ff00
mhceg791-199 <mhceg791@gmail.com>
2025-10-06 14:21:04 +0300

    updated

--- src/components/Solutions/solutuibsposter/solutionsPoster.jsx
+++ src/components/Solutions/solutuibsposter/solutionsPoster.jsx
@@ -9,7 +9,7 @@ function SolutionsPoster() {
         }}
         className="poster  mt-10 "
       >
-        <div className="flex flex-col justify-start items-center p-5 md:h-full">
+        <div className="flex flex-col justify-start items-center p-5 md:h-full text-white">
           <h1 className="custom-text-xxl  md:mt-20 mt-30">
             Structural Engineering Solutions
           </h1>
