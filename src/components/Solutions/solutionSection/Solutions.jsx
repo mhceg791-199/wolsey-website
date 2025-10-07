be91134c793993b5469d84d440cf5e2612c01fe1
mhceg791-199 <mhceg791@gmail.com>
2025-10-07 17:14:31 +0300

    updated

--- src/components/Solutions/solutionSection/Solutions.jsx
+++ src/components/Solutions/solutionSection/Solutions.jsx
@@ -8,12 +8,9 @@ function Solutions() {
           <span className="text-mainBrown pe-2 font-berlin custom-text-xl">
             OUR SOLUTIONS
           </span>
-          <span className="custom-text-lg font-semibold">
-            With over 50 years of expertise to deliver avant-garde solutions in concrete
-          </span>
         </p>
         <p>
-          steel, and timber. Our signature precision integrates rigorous analyses and flawless BIM/REVIT modeling (LOD 300-400), ensuring fabrication-ready accuracy. With 95% client retention across 150+ prestigious partnerships, we bring visionary designs to life.
+          With over 50 years of expertise to deliver avant-garde solutions in concrete steel, and timber. Our signature precision integrates rigorous analyses and flawless BIM/REVIT modeling (LOD 300-400), ensuring fabrication-ready accuracy. With 95% client retention across 150+ prestigious partnerships, we bring visionary designs to life.
         </p>
       </div>
     </>
